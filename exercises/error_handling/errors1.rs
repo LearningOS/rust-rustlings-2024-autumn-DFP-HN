d27669fcaa7c634107a7f1f933411b2e951d5782
root <root@LAPTOP-G7SDFTEL>
2024-10-05 18:16:19 +0800

    modify options,error_handling,generics,quiz2,traits

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -7,9 +7,6 @@
 // it!
 //
 // Execute `rustlings hint errors1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
 
 pub fn generate_nametag_text(name: String) -> Option<String> {
     if name.is_empty() {
@@ -27,7 +24,7 @@ mod tests {
     #[test]
     fn generates_nametag_text_for_a_nonempty_name() {
         assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
+            generate_nametag_text("Beyoncé".into()).map_or(Err("`name` was empty; it must be nonempty."), |v| Ok(v)),
             Ok("Hi! My name is Beyoncé".into())
         );
     }
@@ -35,7 +32,7 @@ mod tests {
     #[test]
     fn explains_why_generating_nametag_text_fails() {
         assert_eq!(
-            generate_nametag_text("".into()),
+            generate_nametag_text("".into()).map_or(Err("`name` was empty; it must be nonempty."), |v| Ok(v)),
             // Don't change this line
             Err("`name` was empty; it must be nonempty.".into())
         );
